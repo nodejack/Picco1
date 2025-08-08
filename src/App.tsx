a4eaff6852dc48347cfb846d16fc932ae5f8823e
[dyad] <projects.hyper@outlook.com>
2025-08-08 20:35:31 +0100

    [dyad] Fixed the provider order to resolve the sidebar context error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <SidebarProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
+      <BrowserRouter>
+        <SidebarProvider>
+          <Toaster />
+          <Sonner />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/predictions" element={<PredictionsPage />} />
@@ -27,8 +27,8 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
-      </SidebarProvider>
+        </SidebarProvider>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
