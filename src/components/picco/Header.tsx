d6d15eb560dadbb0a5e2c1978d48b589c2e2e0fb
[dyad] <projects.hyper@outlook.com>
2025-08-08 21:23:11 +0100

    [dyad] Changed home header to solid dark theme - wrote 1 file(s)

--- src/components/picco/Header.tsx
+++ src/components/picco/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-30 h-16 border-b border-[var(--border-color)] bg-ticker-animated">
+    <header className="fixed top-0 left-0 right-0 z-30 h-16 border-b border-[var(--border-color)] bg-[var(--background-dark)]">
       <div className="container mx-auto flex h-full items-center justify-start px-4">
         <h1 className="text-3xl font-bold text-[var(--text-primary-light)]">Picco</h1>
       </div>
